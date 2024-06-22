b4054838b1075e76248a08f77f7d69c2cea11e20
Roman Makarov <rscm544@gmail.com>
2024-06-22 17:05:57 +0300

    Fixing context length

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from bs4 import BeautifulSoup
 
+from nltk.tokenize import sent_tokenize
+import nltk
+nltk.download('punkt')
+
 # Disable warnings
 logging.set_verbosity_warning()
 
@@ -112,21 +116,16 @@ def compute_prob_single(text, tokenizer, model):
     return int(prob[1] * 100)
 
 
-def paraphrase_single(text, tokenizer, model, lang='en', num_sentences=3, beams=5, grams=4):
+def paraphrase_sentence(sentence, tokenizer, model, lang='en', num_sentences=1, beams=3, grams=4):
     if lang != 'en':
-        x = tokenizer(text, return_tensors='pt', padding=True).to(model.device)
+        x = tokenizer(sentence, return_tensors='pt', padding=True).to(model.device)
         max_size = int(x.input_ids.shape[1] * 1.5 + 10)
 
-        generated_sentences = []
-        for i in range(num_sentences):
-            out = model.generate(**x, encoder_no_repeat_ngram_size=grams, do_sample=True, num_beams=beams,
+        out = model.generate(**x, encoder_no_repeat_ngram_size=grams, do_sample=True, num_beams=beams,
                                  max_length=max_size, no_repeat_ngram_size=4, )
-            generated_sentences.append(tokenizer.decode(out[0], skip_special_tokens=True))
+        return tokenizer.decode(out[0], skip_special_tokens=True)
 
-        return generated_sentences
-
-
-    encoding = tokenizer.encode_plus(text, padding=True, truncation=True, pad_to_max_length=True, return_tensors="pt")
+    encoding = tokenizer.encode_plus(sentence, padding=True, truncation=True, return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"].to(model.device), encoding["attention_mask"].to(model.device)
 
     beam_outputs = model.generate(
@@ -135,20 +134,28 @@ def paraphrase_single(text, tokenizer, model, lang='en', num_sentences=3, beams=
         max_length=int(int(input_ids.shape[1] * 1.5 + 10)),
         top_k=120,
         top_p=0.98,
-        num_beams=beams,
-        num_return_sequences=num_sentences
     )
 
-    final_outputs = []
-    for beam_output in beam_outputs:
-        sent = tokenizer.decode(beam_output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        if sent.lower() != text.lower() and sent not in final_outputs:
-            final_outputs.append(sent)
-
-    return final_outputs
+    return tokenizer.decode(beam_outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
 
-def inference_single(text, num_paraphrases=3):
+def paraphrase_single(text, tokenizer, model, lang='en', beams=5, grams=4):
+    sentences = sent_tokenize(text)
+    rewrites = []
+
+    final_text = ''
+    for sent in sentences:
+        res = paraphrase_sentence(sent, tokenizer, model, lang, beams=beams, grams=grams)
+        rewrites.append(res)
+
+        if type(res) is str:
+            if len(final_text):
+                final_text += ' '
+            final_text += res
+
+    return final_text
+
+def inference_single(text, num_paraphrases=2):
     res = chardet.detect(text.encode('cp1251'))
     lang = 'en'
 
@@ -162,7 +169,7 @@ def inference_single(text, num_paraphrases=3):
         sec_model, sec_tokenizer = par_model, par_tokenizer
 
     basic_prob = compute_prob_single(text, cur_tokenizer, cur_model)
-    paraphrases = paraphrase_single(text, sec_tokenizer, sec_model, lang=lang, num_sentences=num_paraphrases)
+    paraphrases = [paraphrase_single(text, sec_tokenizer, sec_model, lang=lang) for _ in range(num_paraphrases)]
     probabilities = [compute_prob_single(par, cur_tokenizer, cur_model) for par in paraphrases]
 
     both = list(zip(paraphrases, probabilities))
@@ -204,7 +211,6 @@ def inference_change(request: InferenceRequest):
         raise HTTPException(status_code=400, detail="Input text is required")
 
     prob, paraphrases, probabilities = inference_single(text)
-    print(text, prob, paraphrases, probabilities )
     return InferenceResponse(prob=prob, paraphrases=paraphrases, probabilities=probabilities)
     
 
